360b1d56a2a7bf87aaf2626eb60290355280ff71
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2024-06-01 22:45:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use("/api/quiz", quizRoutes);
 //Checking Health Route
 app.get("/", (req, res) => {
   res.json({
-    message: "Checking Health Route",
+    message: "Server up and running",
   });
 });
 
